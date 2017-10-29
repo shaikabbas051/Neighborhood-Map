c5fdc5415f75725a833e874f0316015be020b73b
shaik abbas <shaikabbas051@gmail.com>
2017-10-29 15:23:28 +0530

    separated model.js

--- js/knockout.js
+++ js/knockout.js
@@ -1,63 +1,37 @@
-var models = [
-	{
-		name: 'Charminar',
-		location: {lat: 17.361564,lng: 78.474665},
-		type: 'establishment'
-	},
-	{
-		name: 'Wonderla',
-		location: {lat: 17.217318,lng: 78.529179},
-		type: 'amusement_park'
-	},
-	{
-		name: 'Nehru Zoological Park',
-		location: {lat: 17.351243,lng: 78.449687},
-		type: 'zoo'
-	},
-	{
-		name: 'Golconda Fort',
-		location: {lat: 17.383309,lng: 78.401053},
-		type: 'establishment'
-	},
-	{
-		name: 'Osmania University',
-		location: {lat: 17.410843,lng: 78.528171},
-		type: 'university'
-	},
-	{
-		name: 'Birla Mandir',
-		location: {lat: 17.406237,lng: 78.469060},
-		type: 'hindu_temple'
-	}
-	];
-
 function initMap(){
-
 	var map  = new google.maps.Map(document.getElementById('map'),{
 		center: {lat: 17.385044,lng: 78.486671},
-		zoom: 13
+		zoom: 11
 	});
-	/*for(var i = 0; i < models.length; i++){
-		var marker = new google.maps.Marker({
-			position: models[i].location,
-			title: models[i].name,
-			animation: google.maps.Animation.DROP
-		})
-		var infowindow = new google.maps.InfoWindow({maxWidth: 200});
+	
+	for(var i = 0; i < models.length; i++){
+   		 var titles = models[i].name;
+       	 var positions = models[i].location;
+          //set marker
+          var marker = new google.maps.Marker({
+            position: positions,
+            title: titles,
+            animation: google.maps.Animation.DROP
+            });
+          // push each marker in to markers array
+          marker.setMap(map);
+          //declaring the content
           var content = models[i].name;
-          marker.addListener('click',(function(map, marker, content){
+          //infowindow class to show information
+          var infowindow = new google.maps.InfoWindow({maxWidth: 200});
+          //Event Listener when clicked on marker display info window
+          marker.addListener('click',(function(marker,content){
               return function(){
-	              infowindow.setContent(content);
-	              infowindow.open(map,marker);
-	          };
-            })(map, marker, content));
-	}*/
+              infowindow.setContent(content);
+              infowindow.open(map,marker);};
+            })(marker,content));
+      }
 	ko.applyBindings(new viewModel());
 }
 
 var viewModel = function(data){
-	for(var i = 0; i < models.length; i++){
 	this.list = ko.observableArray();
-	this.list.push(models[i].name);
-}
+	for(var i = 0; i < models.length; i++){
+		this.list.push(models[i].name);
+	};
 }
---
+++ js/model.js
@@ -0,0 +1,47 @@
+var models = [
+	{
+		name: 'Charminar',
+		location: {lat: 17.361564,lng: 78.474665},
+		type: 'establishment'
+	},
+	{
+		name: 'Wonderla',
+		location: {lat: 17.217318,lng: 78.529179},
+		type: 'amusement_park'
+	},
+	{
+		name: 'Nehru Zoological Park',
+		location: {lat: 17.351243,lng: 78.449687},
+		type: 'zoo'
+	},
+	{
+		name: 'Golconda Fort',
+		location: {lat: 17.383309,lng: 78.401053},
+		type: 'establishment'
+	},
+	{
+		name: 'Osmania University',
+		location: {lat: 17.410843,lng: 78.528171},
+		type: 'university'
+	},
+	{
+		name: 'Birla Mandir',
+		location: {lat: 17.406237,lng: 78.469060},
+		type: 'hindu_temple'
+	},
+	{
+		name: 'Spanish Mosque',
+		location: {lat: 17.443758,lng: 78.472292},
+		type: 'mosque'
+	},
+	{
+		name: 'Osman sagar',
+		location: {lat: 17.376266,lng: 78.298864},
+		type: 'park'
+	},
+	{
+		name: 'Rajiv Gandhi International Airport',
+		location: {lat: 17.240263,lng: 78.429385},
+		type: 'airport'
+	}
+	];
