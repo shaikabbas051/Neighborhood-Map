b0261513b76992522129c66e50c1e8676fc95955
shaik abbas <shaikabbas051@gmail.com>
2017-10-31 22:16:51 +0530

    Added description

--- js/model.js
+++ js/model.js
@@ -2,46 +2,55 @@ var models = [
 	{
 		name: 'Charminar',
 		location: {lat: 17.361564,lng: 78.474665},
-		type: 'establishment'
+		type: 'establishment',
+		description:'The Charminar, constructed in 1591, is a monument and mosque located in Hyderabad, Telangana, India. The landmark has become a global icon of Hyderabad, listed among the most recognized structures of India.'
 	},
 	{
 		name: 'Wonderla',
 		location: {lat: 17.217318,lng: 78.529179},
-		type: 'amusement_park'
+		type: 'amusement_park',
+		description: 'Wonderla Holidays, is one of the largest amusement park operators company in India. Wonderla Hyderabad offers 25 land based rides and 18 water based attractions on 50acres of land. Wonderla Amusement Park is located 28 km away from Hyderabad City.'
 	},
 	{
 		name: 'Nehru Zoological Park',
 		location: {lat: 17.351243,lng: 78.449687},
-		type: 'zoo'
+		type: 'zoo',
+		description: 'Nehru Zoological Park is a zoo located near Mir Alam Tank in Hyderabad, Telangana, India. It is one of the most visited destinations in Hyderabad.'
 	},
 	{
 		name: 'Golconda Fort',
 		location: {lat: 17.383309,lng: 78.401053},
-		type: 'establishment'
+		type: 'establishment',
+		description: 'Golkonda, also known as Golconda, Gol konda, or Golla konda, is a citadel and fort in Southern India and was the capital of the medieval sultanate of the Qutb Shahi dynasty, is situated 11 kilometres west of Hyderabad.'
 	},
 	{
 		name: 'Osmania University',
 		location: {lat: 17.410843,lng: 78.528171},
-		type: 'university'
+		type: 'university',
+		description: 'Osmania University is a public state university located in Hyderabad, India, founded in 1918 with the help of chief architect of Mahbub Ali Khan - Nawab Sarwar Jung.'
 	},
 	{
 		name: 'Birla Mandir',
 		location: {lat: 17.406237,lng: 78.469060},
-		type: 'hindu_temple'
+		type: 'hindu_temple',
+		description: 'Birla Mandir is a Hindu temple, built on a 280 feet high hillock called Naubath Pahad on a 13 acres plot. The construction took 10 years and was opened in 1976 by Swami Ranganathananda of Ramakrishna Mission.'
 	},
 	{
 		name: 'Spanish Mosque',
 		location: {lat: 17.443758,lng: 78.472292},
-		type: 'mosque'
+		type: 'mosque',
+		description:'The Spanish Mosque, also known as Masjid Iqbal Ud Daula or Jam e Masjid Aiwan-E-Begumpet, is a mosque within the Paigah Palace Begumpet, Hyderabad, India.'
 	},
 	{
 		name: 'Osman sagar',
 		location: {lat: 17.376266,lng: 78.298864},
-		type: 'park'
+		type: 'park',
+		description:'Osman Sagar, popularly known as Gandipet, is a reservoir in the Indian city of Hyderabad. The lake is around 46 km², and the reservoir is around 29 km², with total level of 1,790 feet and a capacity of 3.9 tmc ft.'
 	},
 	{
 		name: 'Rajiv Gandhi International Airport',
 		location: {lat: 17.240263,lng: 78.429385},
-		type: 'airport'
+		type: 'airport',
+		description:'Rajiv Gandhi International Airport is an international airport that serves Hyderabad, the largest city in the Indian state of Telangana. It is located in Shamshabad, about 24 kilometres south of Hyderabad'
 	}
 	];
