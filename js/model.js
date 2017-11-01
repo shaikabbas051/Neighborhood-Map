9279dceb8c2361bda5cbbfade24495004f61c42e
shaik abbas <shaikabbas051@gmail.com>
2017-11-01 20:04:44 +0530

    corrected place id

--- js/model.js
+++ js/model.js
@@ -3,63 +3,63 @@ var models = [
 		name: 'Charminar',
 		location: {lat: 17.361564,lng: 78.474665},
 		type: 'establishment',
-		id: '6dad76d49810ea0ab8ebb73a7c355df2ac917d30',
+		id: 'ChIJm5MaboqXyzsR-xPxquRpWss',
 		description:'The Charminar is a monument and mosque located in Hyderabad. The landmark has become a global icon of Hyderabad, listed among the most recognized structures of India.'
 	},
 	{
 		name: 'Wonderla',
 		location: {lat: 17.217318,lng: 78.529179},
 		type: 'amusement_park',
-		id: '7edac54dfa680e48a727df239fd689b5bbdb2864',
+		id: 'ChIJM1sJX1ykyzsRdyVvqQVGnN0',
 		description: 'Wonderla Holidays, is one of the largest amusement park operators company in India.'
 	},
 	{
 		name: 'Nehru Zoological Park',
 		location: {lat: 17.351243,lng: 78.449687},
 		type: 'zoo',
-		id: '136a9bd664a8e86928b1399669a4a7b5696a2db8',
+		id: 'ChIJnfoIwMaXyzsRWySGfUS7cXs',
 		description: 'Nehru Zoological Park is a zoo located near Mir Alam Tank in Hyderabad, Telangana, India. It is one of the most visited destinations in Hyderabad.'
 	},
 	{
 		name: 'Golconda Fort',
 		location: {lat: 17.383309,lng: 78.401053},
 		type: 'establishment',
-		id: '3ace9760c3c8e6e4b6c450d3395a8f0a5670d07a',
+		id: 'ChIJ80ITX4yWyzsR34Tevb2pUtc',
 		description: 'Golkonda is a citadel and fort in Southern India and was the capital of the medieval sultanate of the Qutb Shahi dynasty.'
 	},
 	{
 		name: 'Osmania University',
 		location: {lat: 17.410843,lng: 78.528171},
 		type: 'university',
-		id: 'e66d3898741c4131509d57cb44f66bf5584f614e',
+		id: 'ChIJMx5poHGZyzsRTkJqaCoPGX0',
 		description: 'Osmania University is a public state university located in Hyderabad, India, founded in 1918 with the help of chief architect of Mahbub Ali Khan - Nawab Sarwar Jung.'
 	},
 	{
 		name: 'Birla Mandir',
 		location: {lat: 17.406237,lng: 78.469060},
 		type: 'hindu_temple',
-		id: 'eb9dceffe560953782e319954e370c56ea314044',
+		id: 'ChIJBcAk0V2XyzsRjing1vqfQ1k',
 		description: 'Birla Mandir is a Hindu temple, built on a 280 feet high hillock called Naubath Pahad on a 13 acres plot.'
 	},
 	{
 		name: 'Spanish Mosque',
 		location: {lat: 17.443758,lng: 78.472292},
 		type: 'mosque',
-		id: '18b276fa246bbabd46badb25f7c2c4cd0b622b62',
+		id: 'ChIJBbEXt6GQyzsRJV6nWPojr8M',
 		description:'The Spanish Mosque, also known as Masjid Iqbal Ud Daula or Jam e Masjid Aiwan-E-Begumpet, is a mosque within the Paigah Palace Begumpet, Hyderabad, India.'
 	},
 	{
 		name: 'Osman sagar',
 		location: {lat: 17.376266,lng: 78.298864},
 		type: 'park',
-		id: '9333f9f2d77678e26fe7d3c4a801a051d30241a2',
+		id: 'ChIJM9BllNWUyzsRuRPLCQ5Zf1Y',
 		description:'Osman Sagar is a reservoir in the Indian city of Hyderabad.'
 	},
 	{
 		name: 'Rajiv Gandhi International Airport',
 		location: {lat: 17.240263,lng: 78.429385},
 		type: 'airport',
-		id: '8fa2b505503bbeaac35d34f3ee1716d807c9c5ff',
+		id: 'ChIJRRvZsX-7yzsRUajqM-wpIFM',
 		description:'Rajiv Gandhi International Airport is an international airport that serves Hyderabad, the largest city in the Indian state of Telangana. It is located in Shamshabad, about 24 kilometres south of Hyderabad'
 	}
 	];
