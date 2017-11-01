96f5a1ee726cfd15d8f0b3c84873fe8dcce1dc2d
shaik abbas <shaikabbas051@gmail.com>
2017-11-01 19:56:27 +0530

    added animation and ajax

--- js/knockout.js
+++ js/knockout.js
@@ -23,13 +23,15 @@ var viewModel = function(data){
 	   	var titles = models[i].name;
 	 	var positions = models[i].location;
 	 	var description = models[i].description;
-	 	
+	 	var id = models[i].id;
 		//marker object
 		var marker = new google.maps.Marker({
 	  		map: map,
 			position: positions,
 			description: description,
 			title: titles,
+			id: id,
+			timing: '',
 			animation: google.maps.Animation.DROP
 		});//marker end
 		//push each marker in to markers array
@@ -40,7 +42,19 @@ var viewModel = function(data){
 			});
 		});
 	}//end of for loop
-
+	markers().forEach(function(marker){
+		$.ajax({
+			method: 'GET',
+            dataType: "json",
+            url: "https://api.foursquare.com/v2/venues/" + marker.id + "client_id=OLVWHYOR5EMW5JN00JANKRL0R314QJY2JB1QI1R4IDWLUL4J&client_secret=RVBWXZ5JXUVPOWQURKYJBYHB3RAHH0LYSE0HQRLLKXPOAD4C",
+            success: function(data){
+            	marker.timing = data.response.hours.timeframes[0].open[0].start;
+            }
+            /*error: function(e){
+            	alert("Something Wend Wrong");
+            };*/
+		});
+	});
 	
 	//when listItems are clicked, show infowindow of associated marker
 	this.showInfowindow = function(marker) {
@@ -49,11 +63,17 @@ var viewModel = function(data){
 	
 	//set infowindow when click event made on marker or on listItems
 	function populateInfoWindow(marker, infowindow){
-			if(infowindow.marker != marker){
-				infowindow.marker = marker;
-				infowindow.setContent('<strong style="font-size: 20px; font-family: Noto serif, serif">'+marker.title+'</strong>' + '<br>' + '<p style="font-family: Noto serif, serif">'+marker.description+'</p>');
-				infowindow.open(map, marker);
-			}
+		//bounce when marker or listitems are clicked
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+		setTimeout(function() {
+            marker.setAnimation(null);
+        }, 1400);
+		//show infowindow when marker or listitems are clicked
+		if(infowindow.marker != marker){
+			infowindow.marker = marker;
+			infowindow.setContent('<strong style="font-size: 20px; font-family: Noto serif, serif">'+marker.title+'</strong>' + '<br>' + '<p style="font-family: Noto serif, serif">'+marker.description+'</p>' + '<br>' + marker.timing);
+			infowindow.open(map, marker);
+		}
 	}//end populate window
 
 	//observe the input text
@@ -85,6 +105,8 @@ var viewModel = function(data){
         }
     }, this);
 };//end viewModel
-	
+function errorMessage(){
+	alert("Can't load Google Map");
+};
 
  
